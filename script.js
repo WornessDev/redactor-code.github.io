4b06ab3d99ee17827162f7a12b7668448a68d2f4
desertstyddg <66902330+desertstyddg@users.noreply.github.com>
2020-06-19 19:45:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var code = document.getElementById("code");
+	var run = document.getElementById("run");
+	var play = document.getElementById("play");
+	var coding = code.value;
+	function runing () {
+		coding = code.value;
+		document.getElementById('play').innerHTML = coding;
+	}
